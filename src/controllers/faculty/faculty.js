2399d52ded47c867af5d36fd84121359d0aa3a83
Jesus Tortolero <85574712+Jesustortoleroh@users.noreply.github.com>
2026-01-31 23:43:11 -0400

    Refactor faculty routes to use sorting and update views

--- src/controllers/faculty/faculty.js
+++ src/controllers/faculty/faculty.js
@@ -1,12 +1,14 @@
-import { getAllFaculty, getFacultyById } from '../../models/faculty/faculty.js';
+import { getFacultyById, getSortedFaculty } from '../../models/faculty/faculty.js';
 
 // Route handler for the faculty list page
 const facultyListPage = (req, res) => {
-    const faculty = getAllFaculty();
+    const sortBy = req.query.sort || 'deparment';
+    const faculty = getSortedFaculty(sortBy);
 
-    res.render('faculty', {
-        title: 'Faculty',
-        faculty: faculty
+    res.render('faculty/list', {
+        title: 'Faculty Directory',
+        faculty,
+        currentSort: sortBy
     });
 };
 
@@ -15,15 +17,15 @@ const facultyDetailPage = (req, res, next) => {
     const facultyId = req.params.facultyId;
     const member = getFacultyById(facultyId);
 
-    // If faculty member doesn't exist, create 404 error
+    // If faculty doesn't exist, create 404 error
     if (!member) {
-        const err = new Error(`Faculty member ${facultyId} not found`);
+        const err = new Error(`Faculty ${facultyId} not found`);
         err.status = 404;
         return next(err);
     }
 
-    res.render('faculty-detail', {
-        title: `${member.name}`,
+    res.render('faculty/detail', {
+        title: member.name,
         faculty: member
     });
 };
