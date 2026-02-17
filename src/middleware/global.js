86809ed6a0991993d93112ac99275107d25c78d3
Jesustortoleroh <tor19021@byui.edu>
2026-02-16 22:37:33 -0400

    Update Global Middleware

--- src/middleware/global.js
+++ src/middleware/global.js
@@ -81,6 +81,12 @@ const addLocalVariables = (req, res, next) => {
 
     setHeadAssetsFunctionality(res)
     // Continue to the next middleware or route handler
+
+    // Convenience variable for UI state based on session state
+    res.locals.isLoggedIn = false;
+    if (req.session && req.session.user) {
+        res.locals.isLoggedIn = true;
+   }
     next();
 };
 
