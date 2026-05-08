6477e2af22d86c4ed497103451f4fd6a5228ce7d
Jesustortoleroh <tor19021@byui.edu>
2026-05-08 18:44:46 -0400

    Refactoring Course Catalog to MVC Architecture

---
+++ src/controllers/routes.js
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+
+// Create a new router instance
+const router = Router();
+
+// TODO: Add import statements for controllers and middleware
+// TODO: Add route definitions
+
+export default router;
